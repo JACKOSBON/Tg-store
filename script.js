f7c543993c84d67aacdaeb36bf45ff2b1448420f
JACKOSBON <kgamar706@gmail.com>
2025-09-21 22:21:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-function toggleChat() {
-  const chatBox = document.getElementById("chatBox");
-  chatBox.style.display = chatBox.style.display === "flex" ? "none" : "flex";
-}
-
-function sendMessage() {
-  const input = document.getElementById("chatInput");
-  const messages = document.getElementById("messages");
-
-  if (input.value.trim() !== "") {
-    let msg = document.createElement("p");
-    msg.innerHTML = "<b>You:</b> " + input.value;
-    messages.appendChild(msg);
-
-    // Auto reply (demo)
-    let reply = document.createElement("p");
-    reply.innerHTML = "<b>Bot:</b> Thanks for your message!";
-    messages.appendChild(reply);
-
-    messages.scrollTop = messages.scrollHeight;
-    input.value = "";
-  }
+function logout() {
+  alert("You have been logged out!");
+  window.location.href = "login.html"; // Agar login page banaya hai toh usme redirect karega
 }
