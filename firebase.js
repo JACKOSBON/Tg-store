d1e46284eaa3580c9a5012e57cebecb66ebd55ca
JACKOSBON <kgamar706@gmail.com>
2025-09-22 09:51:47 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApI1Bi18rCpVNlXmPoQDb_y28a8kajAwQ",
+  authDomain: "darkstore-f069a.firebaseapp.com",
+  projectId: "darkstore-f069a",
+  storageBucket: "darkstore-f069a.firebasestorage.app",
+  messagingSenderId: "767024328451",
+  appId: "1:767024328451:web:16e4d2993e265f45e00336",
+  measurementId: "G-R1N3F3MM99"
+};
+
+firebase.initializeApp(firebaseConfig);
