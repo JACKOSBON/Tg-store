cf9cc2f12aaf2d617abd1b2bdf08d67a78fe5c05
JACKOSBON <kgamar706@gmail.com>
2025-09-22 09:31:09 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyApI1Bi18rCpVNlXmPoQDb_y28a8kajAwQ",
+  authDomain: "darkstore-f069a.firebaseapp.com",
+  projectId: "darkstore-f069a",
+  storageBucket: "darkstore-f069a.firebasestorage.app",
+  messagingSenderId: "767024328451",
+  appId: "1:767024328451:web:16e4d2993e265f45e00336",
+  measurementId: "G-R1N3F3MM99"
+};
+firebase.initializeApp(firebaseConfig);
