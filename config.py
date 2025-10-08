e6dd8373cef6274b6c46b687fd8c0333f669f4b6
JACKOSBON <kgamar706@gmail.com>
2025-10-08 21:40:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Telegram API credentials - Get from https://my.telegram.org
+API_ID = 22827526
+API_HASH = '571743207a6a973b3c1fa8efbf713432'
+
+# Bot username (without @) that you want to interact with
+TARGET_BOT = 'KiltesAIBot'
+
+# Group where approved messages will be forwarded
+TARGET_GROUP = '@jsjshhsiaia'
+
+# Files
+CODES_FILE = 'codes.txt'
+APPROVED_FILE = 'approved.txt'
+DECLINED_FILE = 'declined.txt'
+
+# Delay between sending codes (in seconds)
+DELAY_BETWEEN_CODES = 30
